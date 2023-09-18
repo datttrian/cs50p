952a53bc65b83891e3b6745a951d549e3e9128ae
Dat Tran <datttrian@gmail.com>
2023-09-18 07:27:05 +0000

    Mon, Sep 18, 2023, 7:27 AM +00:00

---
+++ bank/bank.py
@@ -0,0 +1,18 @@
+def main():
+    # Prompt the user for a greeting
+    user_greeting = input("Enter a greeting: ")
+
+    # Remove leading and trailling whitespace and convert the greeting to lowercase
+    user_greeting = user_greeting.strip().lower()
+
+    # Check if the greeting starts with "hello" and output the corresponding amount
+    if user_greeting_startswith("hello"):
+        print("$0")
+    elif user_greeting.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
+
+
+if __name__ == "__main__":
+    main()
