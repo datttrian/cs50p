7d6b17897738bb0f3fce60d8e6e5e66f60073e08
Dat Tran <datttrian@gmail.com>
2023-09-18 06:28:03 +0000

    Mon, Sep 18, 2023, 6:28 AM +00:00

---
+++ playback/playback.py
@@ -0,0 +1,8 @@
+# Prompt the user for input
+user_input = input("Enter text: ")
+
+# Replace spaces with three periods
+result = user_input.replace(' ', '...')
+
+# Print the modified text
+print(result)
