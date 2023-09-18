89af37d35533d7d4d2cd1bc7f2ad4ada77c6ae96
Dat Tran <datttrian@gmail.com>
2023-09-18 06:48:19 +0000

    Mon, Sep 18, 2023, 6:48 AM +00:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,1 +1,18 @@
+def convert(text):
+    # Replace :) with 🙂
+    text = text.replace(":)", "🙂")
 
+    # Replace :( with 🙁
+    text = text.replace(":(", "🙁")
+
+    return text
+
+
+def main():
+    user_input = input("Enter text: ")
+    converted_text = convert(user_input)
+    print(converted_text)
+
+
+if __name__ == "__main__":
+    main()
