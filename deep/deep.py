28266ed4a9c8955cdc997c2605c80a1dce1857a5
Dat Tran <datttrian@gmail.com>
2023-09-18 07:09:53 +0000

    Mon, Sep 18, 2023, 7:09 AM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -5,5 +5,17 @@ def is_great_question_answer(answer):
     # Check if the answer matches "42", "forty-two", or "forty two"
     return answer == "42" or answer == "forty-two" or answer == "fortytwo"
 
+
 def main():
-    user_input = input("")
+    user_input = input(
+        "What is the Answer to the Great Question of Life, the Universe, and Everything? "
+    )
+
+    if is_greate_question_answer(user_input):
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    main()
