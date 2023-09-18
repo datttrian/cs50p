d4941b1ef8bc4429b3e56fa9d7314bca61108ed5
Dat Tran <datttrian@gmail.com>
2023-09-18 06:19:13 +0000

    Mon, Sep 18, 2023, 6:19 AM +00:00

---
+++ indoor/indoor.py
@@ -0,0 +1,8 @@
+# Prompt the user for input
+user_input = input("Enter text: ")
+
+# Convert the input to lowercase
+lowercase_input = user_input.lower()
+
+# Print the lowercase input
+print(lowercase_input)
