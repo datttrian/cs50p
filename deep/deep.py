4e4cc180834091a0108a6f608d0aedd607753258
Dat Tran <datttrian@gmail.com>
2023-09-18 07:13:26 +0000

    Mon, Sep 18, 2023, 7:13 AM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,17 +1,9 @@
-def is_great_question_answer(answer):
-    # Convert the answer to lowercase and remove spaces
-    answer = answer.lower().replace(" ", "")
-
-    # Check if the answer matches "42", "forty-two", or "forty two"
-    return answer == "42" or answer == "forty-two" or answer == "fortytwo"
-
-
 def main():
-    user_input = input(
-        "What is the Answer to the Great Question of Life, the Universe, and Everything? "
-    )
+    # Read user inut and convert it to lowercase
+    user_input = input().strip().lower()
 
-    if is_greate_question_answer(user_input):
+    # Check if the user input matches any of the accepted responses
+    if user_input == "42" or user_input == "forty-two" or user_input == "forty two":
         print("Yes")
     else:
         print("No")
