90010d5d2079d442c50e4761d0aad6c3d50c37b2
Dat Tran <datttrian@gmail.com>
2023-09-19 06:27:16 +0000

    Tue, Sep 19, 2023, 6:27 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -4,12 +4,13 @@ def main():
 
     # Check if the input is in the correct format
     if len(time_input) != 5 or not time_input[0:2].isdigit() or not time_input[3:].isdigit() or time_input[2] != ":":
-        print("")
+        print("Invalid time format. Please use HH:MM.")
+        return
 
 
-def convert(time):
-...
+# def convert(time):
+# ...
 
 
 if __name__ == "__main__":
-main()
+    main()
