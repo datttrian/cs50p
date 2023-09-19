1071bf80bed9e1742d0deec4a92d28ecdc25820b
Dat Tran <datttrian@gmail.com>
2023-09-19 06:23:44 +0000

    Tue, Sep 19, 2023, 6:23 AM +00:00

---
+++ meal/meal.py
@@ -0,0 +1,14 @@
+def main():
+    # Prompt the user for input
+    time_input = input("Enter the time in 24-hour format (HH:MM): ")
+
+    # Check if the input is in the correct format
+    if len(time_input) != 5 or not time_input[0:2].isdigit() or not time_input[3:].i
+
+
+def convert(time):
+...
+
+
+if __name__ == "__main__":
+main()
