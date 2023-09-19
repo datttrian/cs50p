90179a58fdaed4c45eb7d747c46e091b16395fb7
Dat Tran <datttrian@gmail.com>
2023-09-19 06:24:55 +0000

    Tue, Sep 19, 2023, 6:24 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -3,7 +3,8 @@ def main():
     time_input = input("Enter the time in 24-hour format (HH:MM): ")
 
     # Check if the input is in the correct format
-    if len(time_input) != 5 or not time_input[0:2].isdigit() or not time_input[3:].i
+    if len(time_input) != 5 or not time_input[0:2].isdigit() or not time_input[3:].isdigit() or time_input[2] != ":":
+        print("")
 
 
 def convert(time):
