79418616eb826720d431838f0b0ac2e7ace7c2a9
Dat Tran <datttrian@gmail.com>
2023-09-19 06:30:52 +0000

    Tue, Sep 19, 2023, 6:30 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,39 +1,32 @@
 def convert(time):
     # Split the time string into hours and minutes
     hours, minutes = time.split(":")
-    # Convert hours and minutes to integers
-    hours = int(hours)
-    minutes = int(minutes)
-    # Calculate the total time in hours, including minutes as fractions
-    total_time = hours + (minutes / 60)
-    return total_time
 
+    # Convert hours and minutes to integers and calculate the total hours
+    total_hours = int(hours) + int(minutes) / 60.0
+
+    return total_hours
 
 def main():
-    # Prompt the user for input
-    time_input = input("Enter the time in 24-hour format (HH:MM): ")
+    # Prompt the user for a time in 24-hour format
+    time_str = input("Enter a time in 24-hour format (e.g., 7:00): ")
 
-    # Check if the input is in the correct format
-    if (
-        len(time_input) != 5
-        or not time_input[0:2].isdigit()
-        or not time_input[3:].isdigit()
-        or time_input[2] != ":"
-    ):
-        print("Invalid time format. Please use HH:MM.")
-        return
+    # Convert the input time to total hours as a float
+    try:
+        total_hours = convert(time_str)
 
-    # Convert the input time to a float in hours
-    time_in_hours = convert(time_input)
-
-    # Check if it's mealtime and print the result
-    if 7.0 <= time_in_hours <= 8.0:
-        print("Breakfast time")
-    elif 12.0 <= time_in_hours <= 13.0:
-        print("Lunch time")
-    elif 18.0 <= time_in_hours <= 19.0:
-        print("Dinner time")
-
+        # Check if it's breakfast time, lunch time, or dinner time
+        if 7.0 <= total_hours <= 8.0:
+            print("Breakfast time")
+        elif 12.0 <= total_hours <= 13.0:
+            print("Lunch time")
+        elif 18.0 <= total_hours <= 19.0:
+            print("Dinner time")
+        else:
+            # If it's not mealtime, don't output anything
+            pass
+    except ValueError:
+        print("Invalid time format. Please use 24-hour format (e.g., 7:00).")
 
 if __name__ == "__main__":
     main()
