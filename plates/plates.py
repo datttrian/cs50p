5d8da0f2ef4306dcbd4b038b006d687f970d3fc6
Dat Tran <datttrian@gmail.com>
2023-09-24 18:39:42 +0000

    Sun, Sep 24, 2023, 6:39 PM +00:00

---
+++ plates/plates.py
@@ -0,0 +1,28 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if len(s) < 2 or len(s) > 6:
+        return False
+
+    if not s[:2].isalpha():
+        return False
+
+    if not s[2:].isalnum():
+        return False
+
+    if s[2:].isdigit() and s[2] == "0":
+        return False
+
+    if any(char in ". ,;!@#$%^&*()-_+=<>/\\?:" for char in s):
+        return False
+
+    return True
+
+
+main()
