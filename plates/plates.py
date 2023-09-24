599b1165f220a30f99f52e2e6f6cda648648eb53
Dat Tran <datttrian@gmail.com>
2023-09-24 23:57:58 +0000

    Sun, Sep 24, 2023, 11:57 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -9,25 +9,17 @@ def main():
         print("Invalid")
 
 
-def is_valid(s):
-    # Check if the length of 's' is between 2 and 6 characters (inclusive)
-    if 2 <= len(s) <= 6 and s[:2].isalpha() and s.isalnum():
-        # Iterate through each character in 's'
-        for char in s:
-            # Check if the character is a digit
+def is_valid(b):
+    if 6 >= len(b) >= 2 and b [0:2].isalpha() and b.isalnum():
+        for char in b:
             if char.isdigit():
-                # Get the index of the digit character in 's'
-                index = s.index(char)
-                # Check if all characters from the current index to the end are digits
-                if s[index:].isdigit() and int(char) != 0:
-                    # If all conditions are met, return True
-                    return True
+                index = b.index(char)
+                if b[index:].isdigit()and int(char) !=0:
 
-        # If no valid condition is met, return False
-        return False
-
-    # If the initial conditions are not met, return False
-    return False
+                        return True
+                else:
+                    return False
+        return True
 
 
 if __name__ == "__main__":
