95acb7301254354fe2e7351967d99e5c610d3204
Dat Tran <datttrian@gmail.com>
2023-09-24 23:58:41 +0000

    Sun, Sep 24, 2023, 11:58 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -9,17 +9,28 @@ def main():
         print("Invalid")
 
 
-def is_valid(b):
-    if 6 >= len(b) >= 2 and b [0:2].isalpha() and b.isalnum():
-        for char in b:
-            if char.isdigit():
-                index = b.index(char)
-                if b[index:].isdigit()and int(char) !=0:
-
-                        return True
-                else:
-                    return False
-        return True
+def is_valid(s):
+    # Check if the length of the string is between 2 and 6 characters (inclusive)
+    if 6 >= len(s) >= 2:
+        # Check if the first two characters are alphabetic
+        if s[0:2].isalpha():
+            # Check if the entire string consists of alphanumeric characters
+            if s.isalnum():
+                # Iterate through each character in the string
+                for char in s:
+                    # Check if the character is a digit
+                    if char.isdigit():
+                        # Get the index of the current character in the string
+                        index = s.index(char)
+                        # Check if the remaining substring (starting from the current character) is composed of digits
+                        if s[index:].isdigit() and int(char) != 0:
+                            return True
+                        else:
+                            return False
+                # If there are no digits or all digits are zeros, return True
+                return True
+    # If any of the conditions fail, return False
+    return False
 
 
 if __name__ == "__main__":
