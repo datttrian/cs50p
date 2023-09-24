60ee3d4d02624a394d0fea88627235eae48541ab
Dat Tran <datttrian@gmail.com>
2023-09-24 23:32:50 +0000

    Sun, Sep 24, 2023, 11:32 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -5,18 +5,27 @@ def main():
     else:
         print("Invalid")
 
-def is_valid(s):
-    # Check if the length of the string is between 2 and 6 characters
-    if 2 <= len(s) <= 6:
-        # Check if all characters are uppercase letters or digits
-        if all(char.isalpha() or char.isdigit() for char in s):
-            # Check if the first character is a letter
-            if s[0].isalpha():
-                # Check if numbers are only at the end
-                if s[-1].isdigit() and not s[1:-1].isdigit():
-                    # Check if the first number is not '0'
-                    if s[-1] != '0':
-                        return True
-    return False
 
-main()
+def is_valid(s):
+    # Requirement 1: Must start with at least two letters
+    if len(s) < 2 or not s[:2].isalpha():
+        return False
+
+    # Requirement 2: Maximum of 6 characters (letters or numbers) and a minimum of 2 characters
+    if not (2 <= len(s) <= 6):
+        return False
+
+    # Requirement 3: Numbers must come at the end, and the first number cannot be '0'
+    if not s[-1].isdigit() or s[-1] == "0":
+        return False
+
+    # Requirement 4: No periods, spaces, or punctuation marks are allowed
+    if any(char in s for char in ". ,;:!?"):
+        return False
+
+    # If all requirements are met, the plate is valid
+    return True
+
+
+if __name__ == "__main__":
+    main()
