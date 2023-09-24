08b026582fcbbb33f74428ae5133019cbe067cb0
Dat Tran <datttrian@gmail.com>
2023-09-24 18:54:34 +0000

    Sun, Sep 24, 2023, 6:54 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -5,24 +5,18 @@ def main():
     else:
         print("Invalid")
 
-
 def is_valid(s):
-    if len(s) < 2 or len(s) > 6:
-        return False
-
-    if not s[:2].isalpha():
-        return False
-
-    if not s[2:].isalnum():
-        return False
-
-    if not s[-1].isdigit() or s[2].isdigit() and s[2] == '0':
-        return False
-
-    if any(char in ". ,;!@#$%^&*()-_+=<>/\\?:" for char in s):
-        return False
-
-    return True
-
+    # Check if the length of the string is between 2 and 6 characters
+    if 2 <= len(s) <= 6:
+        # Check if all characters are uppercase letters or digits
+        if all(char.isalpha() or char.isdigit() for char in s):
+            # Check if the first character is a letter
+            if s[0].isalpha():
+                # Check if numbers are only at the end
+                if s[-1].isdigit() and not s[1:-1].isdigit():
+                    # Check if the first number is not '0'
+                    if s[-1] != '0':
+                        return True
+    return False
 
 main()
