d292eeb88572b9770df13b697e5e7755325a5d4d
Dat Tran <datttrian@gmail.com>
2023-09-25 18:38:22 +0000

    Mon, Sep 25, 2023, 6:38 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,43 +1,29 @@
-def get_fraction():
+def main():
     while True:
         try:
             fraction_str = input("Fraction: ")
             x, y = fraction_str.split("/")
             x = int(x)
             y = int(y)
-            if x <= y and y != 0:
-                return x, y
-            else:
-                print("Invalid input. Please enter a valid fraction.")
-        except (ValueError, ZeroDivisionError):
-            print("Invalid input. Please enter a valid fraction.")
 
-
-def calculate_percentage(x, y):
-    percent = (x / y) * 100
-    return round(percent)
-
-
-def main():
-    while True:
-        try:
-            fraction_str = input("Fraction: ")
-            x, y = map(int, fraction_str.split("/"))
-
-            if not (0 <= x <= y):
-                raise ValueError(
-                    "Invalid input. Numerator (X) must be between 0 and the denominator (Y)."
+            if y == 0:
+                print("Input error: Denominator cannot be zero.")
+            elif not (0 <= x <= y):
+                print(
+                    "Input error: Numerator (X) must be between 0 and the denominator (Y)."
                 )
-
-            percent = (x / y) * 100
-            if percent <= 1:
-                print("E")
-            elif percent >= 99:
-                print("F")
             else:
-                print(f"{round(percent)}%")
-        except (ValueError, ZeroDivisionError) as e:
-            print(f"Invalid input. {e}")
+                percent = (x / y) * 100
+                if percent <= 1:
+                    print("E")
+                elif percent >= 99:
+                    print("F")
+                else:
+                    print(f"{round(percent)}%")
+        except ValueError:
+            print(
+                "Input error: Please enter a valid fraction (e.g., X/Y where X and Y are integers)."
+            )
 
 
 if __name__ == "__main__":
