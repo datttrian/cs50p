1eb252fc0711206730b31a5ff59cfb72a9160b61
Dat Tran <datttrian@gmail.com>
2023-09-25 22:11:48 +0000

    Mon, Sep 25, 2023, 10:11 PM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -4,7 +4,23 @@ def main():
 
     try:
         while True:
-            item = input()
+            item = input().strip().lower() # read user input and convert to lowercase
+
+            # Check if the item is already in the dictionary
+            if item in grocery_list:
+                grocery_list[item] += 1
+            else:
+                grocery_list[item] = 1
+
+    except EOFError:
+        pass # end of input
+
+    # Sort the dictionary by keys in a case-insensitive manner
+    sorted_list = sorted(grocery_list.items(), key = lambda x: x[0].casefold())
+
+    # Print the grocery list in the desired format
+    for item, count in sorted list:
+        print(f"{count} {item.upper()}")
 
 
 if __name__ == "__main__":
