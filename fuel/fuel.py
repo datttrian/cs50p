4a8cf5b9eb5aa8d577bc9f250ce70d34387096a6
Dat Tran <datttrian@gmail.com>
2023-09-25 18:41:31 +0000

    Mon, Sep 25, 2023, 6:41 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -4,22 +4,4 @@ def main():
             fraction = input("Fraction: ")
             numerator, denominator = map(int, fraction.split("/"))
 
-            if denominator == 0 or numerator > denominator:
-                raise ValueError
-
-            percent = (numerator / denominator) * 100
-            if percent <= 1:
-                print("E")
-            elif percent >= 99:
-                print("F")
-            else:
-                print(f"{round(percent)}%")
-            break
-        except (ValueError, ZeroDivisionError):
-            print(
-                "Invalid input. Please enter a valid fraction (X/Y) where X and Y are integers."
-            )
-
-
-if __name__ == "__main__":
-    main()
+            
