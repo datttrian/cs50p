e42766e2025a256842aea55bf9d9e82f95a480c7
Dat Tran <datttrian@gmail.com>
2023-09-25 18:31:39 +0000

    Mon, Sep 25, 2023, 6:31 PM +00:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -23,19 +23,10 @@ def main():
         "watermelon": 80,
     }
 
-
-# Prompt the user for input (fruit name)
-user_input = input(
-    "Enter a fruit: "
-).lower()  # convert input to lowercase for case-insensitive matching
-
-# Check if the entered fruit exists in the dictionary
-if user_input in fruit_calories:
-    # If it exists, print the calories for that fruit
-    print(f"Calories: {fruit_calories[user_input]}")
-else:
-    # If it doesn't exist, infor the user that the input is not a recognized fruit
-    pass  # do nothing for invalid input
+    # Prompt the user for input (fruit name)
+    user_input = input(
+        "Enter a fruit: "
+    ).l
 
 if __name__ == "__main__":
     main()
