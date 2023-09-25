0df5b72f31fb991e188d03591cb0471d705fbcd9
Dat Tran <datttrian@gmail.com>
2023-09-25 18:40:12 +0000

    Mon, Sep 25, 2023, 6:40 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,28 +1,23 @@
 def main():
     while True:
         try:
-            fraction_str = input("Fraction: ")
-            x, y = fraction_str.split("/")
-            x = int(x)
-            y = int(y)
+            fraction = input("Fraction: ")
+            numerator, denominator = map(int, fraction.split("/"))
 
-            if y == 0:
-                print("Input error: Denominator cannot be zero.")
-            elif not (0 <= x <= y):
-                print(
-                    "Input error: Numerator (X) must be between 0 and the denominator (Y)."
-                )
+            if denominator == 0 or numerator > denominator:
+                raise ValueError
+
+            percent = (numerator / denominator) * 100
+            if percent <= 1:
+                print("E")
+            elif percent >= 99:
+                print("F")
             else:
-                percent = (x / y) * 100
-                if percent <= 1:
-                    print("E")
-                elif percent >= 99:
-                    print("F")
-                else:
-                    print(f"{round(percent)}%")
-        except ValueError:
+                print(f"{round(percent)}%")
+            break
+        except (ValueError, ZeroDivisionError):
             print(
-                "Input error: Please enter a valid fraction (e.g., X/Y where X and Y are integers)."
+                "Invalid input. Please enter a valid fraction (X/Y) where X and Y are integers."
             )
 
 
