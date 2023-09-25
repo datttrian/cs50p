b922e986883e5f6f6cc71b771217e4c1fc36c46f
Dat Tran <datttrian@gmail.com>
2023-09-25 22:33:29 +0000

    Mon, Sep 25, 2023, 10:33 PM +00:00

--- outdated/outdated copy.py
+++ outdated/outdated copy.py
@@ -1,19 +1,17 @@
-def main():
-    months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ]
-
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
 
 while True:
     date = input("Date: ")
@@ -23,6 +21,7 @@ while True:
         elif "," in date:
             mmdd, yyyy = date.split(", ")
             mm, dd = mmdd.split(" ")
+            # No need to check if mm in months. KeyError is handled separately.
             mm = (months.index(mm)) + 1
         if int(mm) > 12 or int(dd) > 31:
             raise ValueError
