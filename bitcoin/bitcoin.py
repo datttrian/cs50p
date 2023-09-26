6b9dda0f898c27ee4d580bb5bad0019d957bf64b
Dat Tran <datttrian@gmail.com>
2023-09-26 18:27:24 +0000

    Tue, Sep 26, 2023, 6:27 PM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,37 +1,28 @@
 import sys
 import requests
 
-# Check for the command-line argument
-if len(sys.argv) != 2:
-    print("Missing command-line argument")
-    sys.exit(1)
 
-# Try to convert the argument to a float
-try:
-    bitcoins_to_buy = float(sys.argv[1])
-except ValueError:
-    print("Command-line argument is not a valid number")
-    sys.exit(1)
+def main():
+    if len(sys.argv) == 2:
+        try:
+            n = float(sys.argv[1])
+            print(btc_price(n))
+        except ValueError:
+            sys.exit("Command-line argument is not a number")
+    else:
+        sys.exit("Missing command-line argument")
 
-# Make a request to the CoinDesk API
-try:
-    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-    response.raise_for_status()
-except requests.RequestException as e:
-    print(f"Error making API request: {e}")
-    sys.exit(1)
 
-# Parse the JSON response
-try:
-    data = response.json()
-    price_usd = float(data["bpi"]["USD"]["rate"].replace(",", ""))
-except (ValueError, KeyError):
-    print("Error parsing API response")
-    sys.exit(1)
+def btc_price(num):
+    try:
+        response = requests.get(f"https://api.coindesk.com/v1/bpi/currentprice.json")
+        result = response.json()
+        price = result["bpi"]["USD"]["rate_float"]
+        total = price * num
+        return f"${total:,.4f}"
+    except requests.RequestException:
+        return None
 
-# Calculate the total cost
-total_cost = price_usd * bitcoins_to_buy
 
-# Format and print the total cost with thousands separators and four decimal places
-formatted_cost = f"${total_cost:.4f}"
-print(formatted_cost)
+if __name__ == "__main__":
+    main()
