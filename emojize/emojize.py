39b1790e92bbdb91f553570816d237b4f592e905
Dat Tran <datttrian@gmail.com>
2023-09-26 18:00:15 +0000

    Tue, Sep 26, 2023, 6:00 PM +00:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -2,15 +2,9 @@ import emoji
 
 
 def main():
-    input_text = input("Input: ")
-    emojized_text = emojize_text(input_text)
+    user_input = input("Input: ")
+    emojized_text = emoji.emojize(user_input, use_aliases=True)
     print("Output:", emojized_text)
-
-
-def emojize_text(input_text):
-    # Use emoji.emojize to convert emoji codes and aliases to emojis
-    emojized_text = emoji.emojize(input_text, use_aliases=True)
-    return emojized_text
 
 
 if __name__ == "__main__":
