3810ca89e503a18133fa6a225631d1834cae31a9
Dat Tran <datttrian@gmail.com>
2023-09-29 01:32:07 +0000

    Fri, Sep 29, 2023, 1:32 AM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,28 +1,28 @@
 import plates
 
-def test_valid_plate():
+def test_is_valid_plate():
     assert plates.is_valid("AB1234") == True
 
-def test_invalid_length():
+def test_is_invalid_length():
     assert plates.is_valid("ABC") == False
 
-def test_non_alphabetic_characters():
+def test_is_non_alphabetic_characters():
     assert plates.is_valid("123456") == False
 
-def test_non_alphanumeric_characters():
+def test_is_non_alphanumeric_characters():
     assert plates.is_valid("AB$123") == False
 
-def test_non_alpha_start():
+def test_is_non_alpha_start():
     assert plates.is_valid("123AB4") == False
 
-def test_contains_zero():
+def test_is_contains_zero():
     assert plates.is_valid("AB0123") == False
 
-def test_valid_single_digit():
+def test_is_valid_single_digit():
     assert plates.is_valid("A1B23C") == True
 
-def test_valid_long_plate():
+def test_is_valid_long_plate():
     assert plates.is_valid("ABCDE12345") == True
 
-def test_invalid_long_plate():
+def test_is_invalid_long_plate():
     assert plates.is_valid("ABCDEFG12345") == False
