1cd27ab9d401df67c118159444cc9957e24355ee
Dat Tran <datttrian@gmail.com>
2023-09-29 01:36:00 +0000

    Fri, Sep 29, 2023, 1:36 AM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,40 +1,30 @@
-import plates
+from plates import is_valid
 
-def test_is_valid_plate():
-    assert plates.is_valid("AB1234") == True
 
-def test_is_invalid_length():
-    assert plates.is_valid("ABC") == False
+# “All vanity plates must start with at least two letters.”
+def test_begintwoletters():
+    assert is_valid("CS") == True
+    assert is_valid("50") == False
+    assert is_valid("C5") == False
+    assert is_valid("5") == False
 
-def test_is_non_alphabetic_characters():
-    assert plates.is_valid("123456") == False
 
-def test_is_non_alphanumeric_characters():
-    assert plates.is_valid("AB$123") == False
+# “… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
+def test_length():
+    assert is_valid("HICS50") == True
+    assert is_valid("CS") == True
+    assert is_valid("HELLOCS50") == False
 
-def test_is_non_alpha_start():
-    assert plates.is_valid("123AB4") == False
 
-def test_is_contains_zero():
-    assert plates.is_valid("AB0123") == False
+# “Numbers cannot be used in the middle of a plate; they must come at the end.
+#  For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
+# The first number used cannot be a ‘0’.”
+def test_num():
+    assert is_valid("AAA222") == True
+    assert is_valid("AAA22A") == False
+    assert is_valid("AAA022") == False
 
-def test_is_valid_single_digit():
-    assert plates.is_valid("A1B23C") == True
 
-def test_is_valid_long_plate():
-    assert plates.is_valid("ABCDE12345") == True
-
-def test_is_invalid_long_plate():
-    assert plates.is_valid("ABCDEFG12345") == False
-
-if __name__ == "__main__":
-    test_is_valid_plate()
-    test_is_invalid_length()
-    test_is_non_alphabetic_characters()
-    test_is_non_alphanumeric_characters()
-    test_is_non_alpha_start()
-    test_is_contains_zero()
-    test_is_valid_single_digit()
-    test_is_valid_long_plate()
-    test_is_invalid_long_plate()
-    print("All tests passed!")
+# “No periods, spaces, or punctuation marks are allowed.”
+def test_punct():
+    assert is_valid("CS50!") == False
