5d064c01a5eab7c64743ece6db1531a741dd2a3b
Dat Tran <datttrian@gmail.com>
2023-09-29 01:27:19 +0000

    Fri, Sep 29, 2023, 1:27 AM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,30 +1,28 @@
 import plates
 
-# Test cases for the is_valid function
 def test_valid_plate():
     assert plates.is_valid("AB1234") == True
 
 def test_invalid_length():
-    assert plates.is_valid("A") == False
-    assert plates.is_valid("ABCDEFGH") == False
+    assert plates.is_valid("ABC") == False
 
-def test_non_alphabetic_start():
-    assert plates.is_valid("123ABC") == False
-    assert plates.is_valid("12AB34") == False
+def test_non_alphabetic_characters():
+    assert plates.is_valid("123456") == False
 
-def test_non_alphanumeric():
-    assert plates.is_valid("AB#123") == False
-    assert plates.is_valid("AB!123") == False
+def test_non_alphanumeric_characters():
+    assert plates.is_valid("AB$123") == False
 
-def test_invalid_digits():
-    assert plates.is_valid("AB0000") == False
-    assert plates.is_valid("AB0123C") == False
+def test_non_alpha_start():
+    assert plates.is_valid("123AB4") == False
 
-def test_valid_zero_digits():
-    assert plates.is_valid("AB01234") == True
-    assert plates.is_valid("AB0") == True
+def test_contains_zero():
+    assert plates.is_valid("AB0123") == False
 
-if __name__ == "__main__":
-    # Run the tests using pytest
-    import pytest
-    pytest.main()
+def test_valid_single_digit():
+    assert plates.is_valid("A1B23C") == True
+
+def test_valid_long_plate():
+    assert plates.is_valid("ABCDE12345") == True
+
+def test_invalid_long_plate():
+    assert plates.is_valid("ABCDEFG12345") == False
