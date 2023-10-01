07298bf9baa8319127fe01cfd09d164befcc7f42
Dat Tran <datttrian@gmail.com>
2023-10-01 18:07:37 +0000

    Sun, Oct 1, 2023, 6:07 PM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,36 +1,31 @@
-# fuel.py
+def main():
+    frac = input("Fraction: ")
+    pct = convert(frac)
+    print(gauge(pct))
+
 
 def convert(fraction):
-    try:
-        x, y = map(int, fraction.split("/"))
-        if x > y:
-            raise ValueError("X is greater than Y")
-        if y == 0:
-            raise ZeroDivisionError("Y cannot be 0")
-        percentage = round((x / y) * 100)
-        if not 0 <= percentage <= 100:
-            raise ValueError("Percentage out of range")
-        return percentage
-    except (ValueError, ZeroDivisionError):
-        raise ValueError("Invalid input. Please enter a fraction in the format X/Y where X and Y are integers.")
+    x, y = fraction.split("/")
+    if int(x)/int(y) > 1:
+        raise ValueError
+    elif int(y) == 0:
+        raise ZeroDivisionError
+    return int(int(x)/int(y) * 100)
+
 
 def gauge(percentage):
     try:
-        if percentage <= 1:
+        if 0 <= percentage <= 1:
             return "E"
-        elif percentage >= 99:
+        elif 99 <= percentage <= 100:
             return "F"
+        elif 1 < percentage < 99:
+            return f"{int(percentage)}%"
         else:
-            return str(percentage) + "%"
+            raise TypeError
     except TypeError:
-        raise ValueError("Invalid input. Please provide a valid percentage as an integer.")
+        pass
 
-# The rest of your code remains unchanged
-
-def main():
-    x, y = get_fraction()
-    result = calculate_fuel_percentage(x, y)
-    print(result)
 
 if __name__ == "__main__":
     main()
--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,49 +1,24 @@
-# test_fuel.py
+import pytest
+from fuel import convert, gauge
 
-import fuel
 
 def test_convert():
-    # Test valid input
-    assert fuel.convert("3/4") == 75
-    assert fuel.convert("1/2") == 50
+    assert convert("1/2") == 50
+    assert convert("99/100") == 99
+    assert convert("1/100") == 1
 
-    # Test X is greater than Y
-    try:
-        fuel.convert("4/3")
-    except ValueError as e:
-        assert str(e) == "X is greater than Y"
-
-    # Test Y is 0
-    try:
-        fuel.convert("2/0")
-    except ZeroDivisionError as e:
-        assert str(e) == "Y cannot be 0"
-
-    # Test invalid input format
-    try:
-        fuel.convert("invalid")
-    except ValueError as e:
-        assert str(e) == "Invalid input. Please enter a fraction in the format X/Y where X and Y are integers."
 
 def test_gauge():
-    # Test E
-    assert fuel.gauge(0) == "E"
-    assert fuel.gauge(1) == "E"
+    assert gauge(50) == "50%"
+    assert gauge(99) == "F"
+    assert gauge(1) == "E"
 
-    # Test F
-    assert fuel.gauge(99) == "F"
-    assert fuel.gauge(100) == "F"
 
-    # Test Z%
-    assert fuel.gauge(50) == "50%"
-    assert fuel.gauge(75) == "75%"
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
 
-    # Test invalid input
-    try:
-        fuel.gauge("invalid")
-    except ValueError as e:
-        assert str(e) == "Invalid input. Please provide a valid percentage as an integer."
 
-if __name__ == "__main__":
-    import pytest
-    pytest.main()
+def test_value_error():
+    with pytest.raises(ValueError):
+        convert("2/1")
