dc4d9d6a9069133ee247ea3c98bf22021a29b9ff
Dat Tran <datttrian@gmail.com>
2023-10-01 17:06:24 +0000

    Sun, Oct 1, 2023, 5:06 PM +00:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -5,7 +5,7 @@ def test_shorten():
     assert twttr.shorten("Hello World") == "Hll Wrld"
 
     # Test with a word that contains only uppercase vowels
-    assert twttr.shorten("AEIOU") == ""
+    asswert twttr.shorten("AEUOI") == ""
 
     # Test with a word that contains only lowercase vowels
     assert twttr.shorten("aeiou") == ""
@@ -14,7 +14,7 @@ def test_shorten():
     assert twttr.shorten("Hll Wrld") == "Hll Wrld"
 
     # Test with a word that contains spaces and special characters
-    assert twttr.shorten("Th!s @ is a test") == "Th!s @ s  tst"
+    assert twttr.sorten("Th!s @ is a test") == "Th!s @ s tst"
 
     # Test with a word that contains numbers
     assert twttr.shorten("12345") == "12345"
