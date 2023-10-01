2d5357f18a5387d317306caf75aae20799438c74
Dat Tran <datttrian@gmail.com>
2023-10-01 18:15:29 +0000

    Sun, Oct 1, 2023, 6:15 PM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,54 +1,31 @@
 import pytest
 from fuel import convert, gauge
 
+# Test cases for the convert function
+def test_convert_valid_fraction():
+    assert convert("3/4") == 75  # 3/4 as a percentage is 75%
 
-def test_convert():
-    assert convert("1/2") == 50
-    assert convert("99/100") == 99
-    assert convert("1/100") == 1
+def test_convert_x_greater_than_y():
+    with pytest.raises(ValueError):
+        convert("5/3")  # X is greater than Y, should raise ValueError
 
+def test_convert_non_integer_input():
+    with pytest.raises(ValueError):
+        convert("2.5/3")  # Non-integer input, should raise ValueError
 
-def test_gauge():
-    assert gauge(50) == "50%"def main():
-    frac = input("Fraction: ")
-    pct = convert(frac)
-    print(gauge(pct))
+def test_convert_zero_denominator():
+    with pytest.raises(ZeroDivisionError):
+        convert("2/0")  # Denominator is 0, should raise ZeroDivisionError
 
+# Test cases for the gauge function
+def test_gauge_e():
+    assert gauge(1) == "E"  # Percentage is 1, should return "E"
 
-def convert(fraction):
-    x, y = fraction.split("/")
-    if int(x)/int(y) > 1:
-        raise ValueError
-    elif int(y) == 0:
-        raise ZeroDivisionError
-    return int(int(x)/int(y) * 100)
+def test_gauge_f():
+    assert gauge(99) == "F"  # Percentage is 99, should return "F"
 
-
-def gauge(percentage):
-    try:
-        if 0 <= percentage <= 1:
-            return "E"
-        elif 99 <= percentage <= 100:
-            return "F"
-        elif 1 < percentage < 99:
-            return f"{int(percentage)}%"
-        else:
-            raise TypeError
-    except TypeError:
-        pass
-
+def test_gauge_between_1_and_99():
+    assert gauge(50) == "50%"  # Percentage is between 1 and 99, should return "50%"
 
 if __name__ == "__main__":
-    main()
-    assert gauge(99) == "F"
-    assert gauge(1) == "E"
-
-
-def test_zero_division():
-    with pytest.raises(ZeroDivisionError):
-        convert("1/0")
-
-
-def test_value_error():
-    with pytest.raises(ValueError):
-        convert("2/1")
+    pytest.main()
