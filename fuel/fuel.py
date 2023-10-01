55c6447be3ce8a2b331c883c7887e304dd6d3f52
Dat Tran <datttrian@gmail.com>
2023-10-01 17:53:03 +0000

    Sun, Oct 1, 2023, 5:53 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -10,17 +10,20 @@ def get_fraction():
 
             # Check if the fraction is valid (x <= y and y > 0)
             if x <= y and y > 0:
-                return x,y
+                return x, y
             else:
                 print("Invalid fraction. Please try again.")
 
             # Handle exceptions that can occur during input or conversion
-        except: (ValueError, ZeroDivisionError):
-        print("In")
+        except (ValueError, ZeroDivisionError):
+            print(
+                "Invalid input. Please enter a fraction in the format X/Y where X and Y are integers."
+            )
+
 
 # This function calculates the fuel percentage based on x and y values
 def calculate_fuel_percentage(x, y):
-    percentage = (x/y) * 100
+    percentage = (x / y) * 100
 
     # Check the percentage and return appropriate values
     if percentage <= 1:
@@ -30,18 +33,19 @@ def calculate_fuel_percentage(x, y):
     else:
         return str(round(percentage)) + "%"
 
+
 # The main function to execute the program
 def main():
     # Get a valid fraction from the user
     x, y = get_fraction()
 
     # Calculate the fuel percentage
-    result = calculate_fuel(x,y)
+    result = calculate_fuel(x, y)
 
     # Display the result
     print(result)
 
+
 # This block ensures that the main function is executed when the script is run
 if __name__ == "__main__":
     main()
-
