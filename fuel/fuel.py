15e139e33af8dfafed80a41c2feb679f55b3f8fc
Dat Tran <datttrian@gmail.com>
2023-10-01 18:09:45 +0000

    Sun, Oct 1, 2023, 6:09 PM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,31 +1,51 @@
+# This function is defined to get a valid fraction from the user
+def get_fraction():
+    while True:
+        try:
+            # Prompt the user for input and store it in the 'fraction' variable
+            fraction = input("Fraction: ")
+
+            # Split the input by the '/' character and convert the parts to integers
+            x, y = map(int, fraction.split("/"))
+
+            # Check if the fraction is valid (x <= y and y > 0)
+            if x <= y and y > 0:
+                return x, y
+            else:
+                print("Invalid fraction. Please try again.")
+
+            # Handle exceptions that can occur during input or conversion
+        except (ValueError, ZeroDivisionError):
+            print(
+                "Invalid input. Please enter a fraction in the format X/Y where X and Y are integers."
+            )
+
+
+# This function calculates the fuel percentage based on x and y values
+def calculate_fuel_percentage(x, y):
+    percentage = (x / y) * 100
+
+    # Check the percentage and return appropriate values
+    if percentage <= 1:
+        return "E"
+    elif percentage >= 99:
+        return "F"
+    else:
+        return str(round(percentage)) + "%"
+
+
+# The main function to execute the program.
 def main():
-    frac = input("Fraction: ")
-    pct = convert(frac)
-    print(gauge(pct))
+    # Get a valid fraction from the user.
+    x, y = get_fraction()
+
+    # Calculate the fuel percentage.
+    result = calculate_fuel_percentage(x, y)
+
+    # Display the result.
+    print(result)
 
 
-def convert(fraction):
-    x, y = fraction.split("/")
-    if int(x)/int(y) > 1:
-        raise ValueError
-    elif int(y) == 0:
-        raise ZeroDivisionError
-    return int(int(x)/int(y) * 100)
-
-
-def gauge(percentage):
-    try:
-        if 0 <= percentage <= 1:
-            return "E"
-        elif 99 <= percentage <= 100:
-            return "F"
-        elif 1 < percentage < 99:
-            return f"{int(percentage)}%"
-        else:
-            raise TypeError
-    except TypeError:
-        pass
-
-
+# This block ensures that the main function is executed when the script is run.
 if __name__ == "__main__":
     main()
