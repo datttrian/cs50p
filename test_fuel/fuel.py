63fbd5a4c71c1d7cf2afde960bd27d31d747de4c
Dat Tran <datttrian@gmail.com>
2023-10-01 18:13:34 +0000

    Sun, Oct 1, 2023, 6:13 PM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,31 +1,37 @@
 def main():
-    frac = input("Fraction: ")
-    pct = convert(frac)
-    print(gauge(pct))
+    fraction_str = input("Fraction: ")
+    try:
+        percentage = convert(fraction_str)
+        result = gauge(percentage)
+        print(result)
+    except (ValueError, ZeroDivisionError) as e:
+        print(e)
 
+def convert(fraction_str):
+    try:
+        # Split the input by the '/' character and convert the parts to integers
+        x, y = map(int, fraction_str.split("/"))
 
-def convert(fraction):
-    x, y = fraction.split("/")
-    if int(x)/int(y) > 1:
-        raise ValueError
-    elif int(y) == 0:
-        raise ZeroDivisionError
-    return int(int(x)/int(y) * 100)
-
+        # Check if X and Y are integers and Y is not 0
+        if isinstance(x, int) and isinstance(y, int) and y != 0:
+            # Check if X is less than or equal to Y
+            if x <= y:
+                percentage = (x / y) * 100
+                return round(percentage)
+            else:
+                raise ValueError("X must be less than or equal to Y.")
+        else:
+            raise ValueError("X and Y must be integers, and Y must not be 0.")
+    except (ValueError, ZeroDivisionError) as e:
+        raise e
 
 def gauge(percentage):
-    try:
-        if 0 <= percentage <= 1:
-            return "E"
-        elif 99 <= percentage <= 100:
-            return "F"
-        elif 1 < percentage < 99:
-            return f"{int(percentage)}%"
-        else:
-            raise TypeError
-    except TypeError:
-        pass
-
+    if percentage <= 1:
+        return "E"
+    elif percentage >= 99:
+        return "F"
+    else:
+        return str(percentage) + "%"
 
 if __name__ == "__main__":
     main()
