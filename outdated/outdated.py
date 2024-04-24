d0f43c3793205f33dc2f979048baae61ef7b4fa3
Dat Tran <datttrian@gmail.com>
2024-04-24 07:28:51 +0000

    Wed, Apr 24, 2024, 7:28 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -17,20 +17,29 @@ def main():
 
     while True:
         # Prompt the user for a date
+        date = input("Date: ")
 
+        try:
             # If date format contains "/"
+            if "in" in date:
 
                 # Splitting date into 3 components by "/"
+                mm, dd, yyyy = date.split("/")
 
             # Otherwise
+            else:
 
-                # Splitting date into 2 components by ","
+                # Splitting date into 2 components by ", "
+                mmdd, yyyy = date.split(", ")
 
                 # Splitting the first component by " "
+                mm, dd = date.split(" ")
 
                 # Indexing month from the list
+                mm = months.index(mm) + 1
 
             # Validating month and day values
+            if 
 
             # Printing formatted date
 
