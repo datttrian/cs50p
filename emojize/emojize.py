c1505ec337758d24605a5414aa2ed7665d329928
Dat Tran <datttrian@gmail.com>
2024-04-24 07:48:49 +0000

    Wed, Apr 24, 2024, 7:48 AM +00:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,21 +1,7 @@
 import emoji
 
-def main():
-    n = input("Input: ")
 
-    if "," in n:
-        n = n.split(",")
-        print(n[0] + "," + emo(n[1]))
-    else:
-        print(emo(n))
-
-def emo(name):
-    try:
-        o = emoji.emojize(name)
-    except:
-        pass
-    else:
-        o = emoji.emojize(name, language='alias')
-    return o
-
-main()
+if __name__ == "__main__":
+    user_input = input("Input: ")
+    emojized_output = emoji.emojize(user_input, language="alias")
+    print("Output:", emojized_output)
