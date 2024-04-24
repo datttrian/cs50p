a46464fc0668eec1348bb65ba63c3b92571abc27
Dat Tran <datttrian@gmail.com>
2024-04-24 07:11:12 +0000

    Wed, Apr 24, 2024, 7:11 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,5 +1,4 @@
 def main():
-    # Define a list of months
     months = [
         "January",
         "February",
@@ -16,30 +15,23 @@ def main():
     ]
 
     while True:
-        # Continuously prompt the user for a date until manually stopped
         date = input("Date: ")
 
         try:
-            # If the input date uses "/" as a separator
             if "/" in date:
                 mm, dd, yyyy = date.split("/")
 
-            # Otherwise
             else:
                 mmdd, yyyy = date.split(", ")
                 mm, dd = mmdd.split(" ")
                 mm = (months.index(mm)) + 1
 
-            # Check for invalid month or day values
             if int(mm) > 12 or int(dd) > 31:
                 raise ValueError
         except (AttributeError, ValueError, NameError, KeyError):
-            # Catch various exceptions that might occur during input processing and validation
             pass
-        else:
-            # If no exceptions were raised, the date is valid, so print it in the yyyy-mm-dd format
-            print(f"{int(yyyy)}-{int(mm):02}-{int(dd):02}")
-            break
+
+        print(f"{int(yyyy)}-{int(mm):02}-{int(dd):02}")
 
 
 if __name__ == "__main__":
