e7573d6f1d3cbfb97015d1e791a73bc15fcb5136
Dat Tran <datttrian@gmail.com>
2024-04-25 09:20:03 +0000

    Thu, Apr 25, 2024, 9:20 AM +00:00

--- game/game.py
+++ game/game.py
@@ -1,25 +1,31 @@
 from random import randint
 
-while True:
-    try:
-        n = int(input("Level: "))
-        if n <= 0:
-            raise ValueError
-        break
-    except ValueError:
-        print("Invalid input. Please enter a positive integer.")
 
-x = randint(1, n)
-
-while True:
-    try:
-        g = int(input("Guess: "))
-        if g == x:
-            print("Just right!")
+def main():
+    while True:
+        try:
+            n = int(input("Level: "))
+            if n <= 0:
+                raise ValueError
             break
-        elif g < x:
-            print("Too small!")
-        else:
-            print("Too large!")
-    except ValueError:
-        print("Invalid input. Please enter an integer.")
+        except ValueError:
+            print("Invalid input. Please enter a positive integer.")
+
+    x = randint(1, n)
+
+    while True:
+        try:
+            g = int(input("Guess: "))
+            if g == x:
+                print("Just right!")
+                break
+            elif g < x:
+                print("Too small!")
+            else:
+                print("Too large!")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+
+if __name__ == "__main__":
+    main()
