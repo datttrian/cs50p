d21c2f299d0af1225811052d333a7feda034b722
datttrian <datttrian@gmail.com>
2024-04-26 22:03:17 +0000

    Fri, Apr 26, 2024, 10:03 PM +00:00

--- professor/professor.py
+++ professor/professor.py
@@ -2,12 +2,15 @@ import random
 
 
 def main():
+    # Get the difficulty level from the user
     level = get_level()
     error = 0
     score = 0
+    # Loop through 10 rounds of addition questions
     for _ in range(0, 10):
         X = generate_integer(level)
         Y = generate_integer(level)
+        # Loop until the user provides a valid integer input
         while True:
             try:
                 temp = int(input(f"{X} + {Y} = "))
@@ -16,6 +19,7 @@ def main():
                 print("EEE")
                 error += 1
                 break
+        # Check if the user's answer is correct
         if temp != X + Y:
             print("EEE")
             while True:
@@ -41,19 +45,21 @@ def main():
 
 
 def get_level():
+    # Prompt the user to input the difficulty level
     while True:
         try:
-            l = int(input("Level: "))
+            level = int(input("Level: "))
         except ValueError:
             pass
         else:
-            if l == 1 or l == 2 or l == 3:
+            if level == 1 or level == 2 or level == 3:
                 return l
             else:
                 pass
 
 
 def generate_integer(level):
+    # Generate random integers based on the difficulty level
     if level == 1:
         return random.randint(0, 9)
     elif level == 2:
