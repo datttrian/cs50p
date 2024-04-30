29cd204f6f6aaf71096b27392d7d2275c7b47943
datttrian <datttrian@gmail.com>
2024-04-30 09:31:29 +0000

    Tue, Apr 30, 2024, 9:31 AM +00:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -6,8 +6,8 @@ import pytest
     ("input_str", "expected"),
     [
         ("bcdfg", "bcdfg"),  # without vowel replacement
-        ("Python 3.9", "Pythn 3.9"),  # mixed letters and numbers
-        ("", ""),  # an empty string
+        ("aeiouAEIOU", ""),  # without capitalized vowel replacement
+        ("Python 3.9", "Pythn 3.9"),  # omitting numbers
         ("Hey! How are you?", "Hy! Hw r y?"),  # punctuation included
         ("beautiful", "btfl"),  # all lowercase vowels present in the input
     ],
