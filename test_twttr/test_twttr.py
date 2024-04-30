9f5ce5ec6f2b1c0ddec49c700dd645b7ba26c543
datttrian <datttrian@gmail.com>
2024-04-30 09:28:59 +0000

    Tue, Apr 30, 2024, 9:28 AM +00:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -5,14 +5,12 @@ import pytest
 @pytest.mark.parametrize(
     ("input_str", "expected"),
     [
-        ("bcdfg", "bcdfg"),
-        ("aeiouAEIOU", ""),
-        ("Hello World", "Hll Wrld"),
-        ("Python 3.9", "Pythn 3.9"),
-        ("", ""),
-        ("Hey! How are you?", "Hy! Hw r y?"),
-        ("beautiful", "btfl"),
+        ("bcdfg", "bcdfg"),  # without vowel replacement
+        ("Python 3.9", "Pythn 3.9"),  # mixed letters and numbers
+        ("", ""),  # an empty string
+        ("Hey! How are you?", "Hy! Hw r y?"),  # punctuation included
+        ("beautiful", "btfl"),  # all lowercase vowels present in the input
     ],
 )
-def test_twttr(input_str, expected):
+def test_isAnagram(input_str, expected):
     assert shorten(input_str) == expected
