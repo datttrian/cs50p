9fbe7ac9d0492bafb0b5aad6152a1e5bfa3567b0
datttrian <datttrian@gmail.com>
2024-04-30 09:58:18 +0000

    Tue, Apr 30, 2024, 9:58 AM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -8,11 +8,7 @@ import pytest
         ("IRONMAN", False),  # without length checks
         ("ab0123", False),  # without checks for zero placement
         ("xx,.&!", False),  # without checks for alphanumeric characters
-
-        # ("07bond", False),  # checks for zero placement and number placement
-        ("xy420z", False),  # alphanumeric character checks and number placement
-        ("c1ix", False),  # alphanumeric character checks and length checks
-
+        ("xy420z", False),  # without checks for number placement
         # ("ON1123", True),  # length and number placement checks
         # ("hadley", True),  # beginning alphabetical checks
         # ("dddd", True),  # beginning alphabetical checks
@@ -22,6 +18,8 @@ import pytest
         # ("abc69xyz", False),  # alphanumeric character checks
         # ("cs50p", False),  # alphanumeric character checks
         # ("bl <42>", False),  # checks for alphanumeric characters
+        # ("07bond", False),  # checks for zero placement and number placement
+        # ("c1ix", False),  # alphanumeric character checks and length checks
     ],
 )
 def test_is_valid(input_str, expected):
