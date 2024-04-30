ba43aff514fe73c2696b755181e1bde3cf1e566c
datttrian <datttrian@gmail.com>
2024-04-30 09:01:48 +0000

    Tue, Apr 30, 2024, 9:01 AM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -5,15 +5,22 @@ import pytest
 @pytest.mark.parametrize(
     ("input_str", "expected"),
     [
-        ("", False),  # Invalid: empty string
-        ("123", False),  # Invalid: only digits
-        ("Ab1234", False),  # Invalid: too many digits
-        ("A1", False),  # Invalid: only one letter
-        ("A!23", False),  # Invalid: contains a non-alphanumeric character
-        ("aB123", False),  # Invalid: lowercase letter
-        ("A123B", False),  # Invalid: letters not at the beginning
-        ("!Ab123", False),  # Invalid: starts with a non-alphanumeric character
-        ("A123B!", False),  # Invalid: ends with a non-alphanumeric character
+        ("ON1123", True),
+        ("hadley", True),
+        ("AsLongAsYouLoveMe", False),
+        ("c", False),
+        ("abc69xyz", False),
+        ("cs50p", False),
+        ("ab0123", False),
+        ("xy420z", False),
+        ("mnp,!&.", False),
+        ("bl <42>", False),
+        ("07bond", False),
+        ("c1ix", False),
+        ("dddd", True),
+        ("2468", False),
+        ("IRONMAN", False),
+        ("CS15", True),
     ],
 )
 def test_is_valid(input_str, expected):
