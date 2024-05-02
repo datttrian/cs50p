0968d3402b8ba86ef8743857e58cee28cacb0975
datttrian <datttrian@gmail.com>
2024-05-02 09:19:06 +0000

    Thu, May 2, 2024, 9:19 AM +00:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -10,26 +10,28 @@ def main():
     output_file = sys.argv[2]
 
     try:
+        listofdict = []
         # Attempt to open the input CSV file
         with open(input_file, newline='') as infile:
             # Open the output CSV file for writing
             with open(output_file, 'w', newline='') as outfile:
                 reader = csv.DictReader(infile)
+
+                # Iterate through each row of the input CSV file
+                for row in reader:
+                    # Split the 'name' field into first name and last name
+                    first_name, last_name = row['name'].split(', ')
+                    listofdict.append({"first": last_name, "last": first_name, "house": row["house"]})
+
                 fieldnames = ['first', 'last', 'house']
                 writer = csv.DictWriter(outfile, fieldnames=fieldnames)
 
                 # Write the header to the output CSV file
                 writer.writeheader()
 
-                # Iterate through each row of the input CSV file
-                for row in reader:
-                    # Split the 'name' field into first name and last name
-                    first_name, last_name = row['name'].split(', ')
-
-                    # Write the modified row to the output CSV file
-                    writer.writerow({'first': first_name, 'last': last_name, 'house': row['house']})
-
-                print("Conversion complete!")
+                # Write the modified row to the output CSV file
+                for i in range(len(listofdict)):
+                    writer.writerow({"first": listofdict[i]["first"], "last": listofdict[i]["last"], "house": listofdict[i]["house"]})
 
     except FileNotFoundError:
         sys.exit(f"Could not read {input_file}")
