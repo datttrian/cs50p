72cde478497bc174460a65414f05b5c4d608e22f
datttrian <datttrian@gmail.com>
2024-05-02 09:22:06 +0000

    Thu, May 2, 2024, 9:22 AM +00:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 
+
 def main():
     # Check if correct number of command-line arguments provided
     if len(sys.argv) != 3:
@@ -10,31 +11,39 @@ def main():
     output_file = sys.argv[2]
 
     try:
-        listofdict = []
+        modified_records = []
         # Attempt to open the input CSV file
-        with open(input_file, newline='') as infile:
+        with open(input_file, newline="") as infile:
             # Open the output CSV file for writing
-            with open(output_file, 'w', newline='') as outfile:
-                reader = csv.DictReader(infile)
+            reader = csv.DictReader(infile)
 
-                # Iterate through each row of the input CSV file
-                for row in reader:
-                    # Split the 'name' field into first name and last name
-                    first_name, last_name = row['name'].split(', ')
-                    listofdict.append({"first": last_name, "last": first_name, "house": row["house"]})
+            # Iterate through each row of the input CSV file
+            for row in reader:
+                # Split the 'name' field into first name and last name
+                first_name, last_name = row["name"].split(", ")
+                modified_records.append(
+                    {"first": last_name, "last": first_name, "house": row["house"]}
+                )
+        with open(output_file, "w", newline="") as outfile:
+            fieldnames = ["first", "last", "house"]
+            writer = csv.DictWriter(outfile, fieldnames=fieldnames)
 
-                fieldnames = ['first', 'last', 'house']
-                writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+            # Write the header to the output CSV file
+            writer.writeheader()
 
-                # Write the header to the output CSV file
-                writer.writeheader()
-
-                # Write the modified row to the output CSV file
-                for i in range(len(listofdict)):
-                    writer.writerow({"first": listofdict[i]["first"], "last": listofdict[i]["last"], "house": listofdict[i]["house"]})
+            # Write the modified row to the output CSV file
+            for i in range(len(modified_records)):
+                writer.writerow(
+                    {
+                        "first": modified_records[i]["first"],
+                        "last": modified_records[i]["last"],
+                        "house": modified_records[i]["house"],
+                    }
+                )
 
     except FileNotFoundError:
         sys.exit(f"Could not read {input_file}")
+
 
 if __name__ == "__main__":
     main()
