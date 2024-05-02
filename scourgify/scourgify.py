f3097d5db9bebf5ccdb7208e23362256dd9f8220
datttrian <datttrian@gmail.com>
2024-05-02 09:36:04 +0000

    Thu, May 2, 2024, 9:36 AM +00:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -6,34 +6,42 @@ def main():
     # Expect exactly two command-line argument CSV file names
     if len(sys.argv) != 3:
         sys.exit("Usage: python scourgify.py input.csv output.csv")
-
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
     try:
-        modified_records = []
+
         # Open the input CSV file
         with open(input_file, newline="") as infile:
+
+            # Reading input CSV file as a dictionary
             reader = csv.DictReader(infile)
 
-            # Iterate through each row of the input CSV file
+            # Initializing an empty list to store dictionaries
+            modified_records = []
+
+            # Modify the rows of the input CSV file
             for row in reader:
+
                 # Split the 'name' field into first name and last name
                 first_name, last_name = row["name"].split(", ")
-                # 
+
+                # Appending the dictionary containing first name, last name, and house to the list
                 modified_records.append(
                     {"first": last_name, "last": first_name, "house": row["house"]}
                 )
 
-        # Open the output CSV file for writing
+        # Open the output CSV file
         with open(output_file, "w", newline="") as outfile:
+
+            # Create a CSV writer object with specified columns
             columns = ["first", "last", "house"]
             writer = csv.DictWriter(outfile, columns=columns)
 
             # Write the header to the output CSV file
             writer.writeheader()
 
-            # Write the modified row to the output CSV file
+            # Write the modified rows to the output CSV file
             for i in range(len(modified_records)):
                 writer.writerow(
                     {
