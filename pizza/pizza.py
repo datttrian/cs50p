3f17a73bafdb9247e7d872d3ae5c923d154940bb
datttrian <datttrian@gmail.com>
2024-05-02 08:41:37 +0000

    Thu, May 2, 2024, 8:41 AM +00:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -3,22 +3,6 @@ import csv
 from tabulate import tabulate
 
 
-def read_csv(file_name):
-    try:
-        with open(file_name, newline="") as csvfile:
-            reader = csv.reader(csvfile)
-            data = list(reader)
-        return data
-    except FileNotFoundError:
-        sys.exit("File does not exist")
-
-
-def print_pizza_table(data):
-    headers = data[0]
-    rows = data[1:]
-    print(tabulate(rows, headers=headers, tablefmt="grid"))
-
-
 def main():
     if len(sys.argv) != 2:
         sys.exit("Too few or too many command-line arguments")
@@ -28,9 +12,16 @@ def main():
     if not file_name.endswith(".csv"):
         sys.exit("Not a CSV file")
 
-    pizza_data = read_csv(file_name)
+    try:
+        with open(file_name, newline="") as csvfile:
+            reader = csv.reader(csvfile)
+            data = list(reader)
+    except FileNotFoundError:
+        sys.exit("File does not exist")
 
-    print_pizza_table(pizza_data)
+    headers = data[0]
+    rows = data[1:]
+    print(tabulate(rows, headers=headers, tablefmt="grid"))
 
 
 if __name__ == "__main__":
