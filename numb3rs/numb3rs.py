436d037803c7c6c5a790471f581b9a1e98057f2a
datttrian <datttrian@gmail.com>
2024-05-03 08:47:44 +0000

    Fri, May 3, 2024, 8:47 AM +00:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -6,6 +6,7 @@ def main():
 
 
 def validate(ip):
+    # Regular expression pattern for IPv4 address
     pattern = r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
     if re.match(pattern, ip):
         return True
---
+++ numb3rs/test_numb3rs.py
@@ -0,0 +1,13 @@
+from numb3rs import validate
+
+
+def test_valid_ipv4():
+    assert validate("127.0.0.1") == True
+    assert validate("255.255.255.0") == True
+
+
+def test_invalid_ipv4():
+    assert validate("275.3.6.28") == False
+    assert validate("512.512.512.512") == False
+    assert validate("1.2.3.1000") == False
+    assert validate("cat") == False
